c52d4115c0de2e38e02e9d433e785ca597545915
MasteRminD6666 <ramikhzaitoun@gmail.com>
2022-04-05 11:39:50 +0300

    editing store

--- packages/Layout/store/index.js
+++ packages/Layout/store/index.js
@@ -1,13 +1,13 @@
 import Vuex from "vuex";
 import Layout from "./modules/mainLayout";
-import Notifications from "../../../notifications.agentsoncloud.com/store/modules/notifications";
-import allConsumers from "../../../consumers.agentsoncloud.com/store/modules/allConsumers";
-import blackListManegment from "../../../consumers.agentsoncloud.com/store/modules/blackListManegment";
-import serviceProviders from "../../../consumers.agentsoncloud.com/store/modules/serviceProviders";
-import services from "../../../consumers.agentsoncloud.com/store/modules/services";
-import inventory from '../../../inventory.agentsoncloud.com/store/modules/inventory';
-import createAppointment from '../../../clinic.management.agentsoncloud.com/store/modules/createappointment'
-import updateAppointment from '../../../clinic.management.agentsoncloud.com/store/modules/updateappointment'
+import Notifications from "../../notifications.agentsoncloud.com/store/modules/notifications";
+import allConsumers from "../../consumers.agentsoncloud.com/store/modules/allConsumers";
+import blackListManegment from "../../consumers.agentsoncloud.com/store/modules/blackListManegment";
+import serviceProviders from "../../consumers.agentsoncloud.com/store/modules/serviceProviders";
+import services from "../../consumers.agentsoncloud.com/store/modules/services";
+import inventory from '../../inventory.agentsoncloud.com/store/modules/inventory';
+import createAppointment from '../../clinic.management.agentsoncloud.com/store/modules/createappointment'
+import updateAppointment from '../../clinic.management.agentsoncloud.com/store/modules/updateappointment'
 
 const createStore = () => {
   return new Vuex.Store({
